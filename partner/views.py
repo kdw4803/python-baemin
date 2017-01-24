7f8f44a992f5d056ea5ac5f65b450abe1faf1a2f
Brad <brad@Bradui-MacBook-Pro.local>
2017-01-24 22:41:02 +0900

    update partner page

--- partner/views.py
+++ partner/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from .forms import PartnerForm
 
 # Create your views here.
+def index(request):
+
+    ctx = {}
+    if request.method == "GET":
+        partner_form = PartnerForm()
+        ctx.update({"form": partner_form})
+    elif request.method == "POST":
+        partner_form = PartnerForm(request.POST)
+        if partner_form.is_valid():
+            partner = partner_form.save(commit=False)
+            partner.user = request.user
+            partner.save()
+            return redirect("/partner/")
+        else:
+            ctx.update({"form": partner_form})
+
+    return render(request, "index.html", ctx)
+
+def login(request):
+    ctx = {}
+
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect("/partner/")
+        else:
+            ctx.update({"error":"사용자가 없습니다."})
+
+    return render(request, "login.html", ctx)
+
+def signup(request):
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = User.objects.create_user(username, email, password)
+
+    ctx = {}
+    return render(request, "signup.html", ctx)
+
+def logout(request):
+    auth_logout(request)
+    return redirect("/partner/")
